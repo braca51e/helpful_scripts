977d0ac98d98868cf914ccd42ace7b8ed53d1513
braca51e <braca51e@gmail.com>
2018-03-18 17:14:54 -0400

    install opencv ubuntu

---
+++ install_opencv.sh
@@ -0,0 +1,45 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get remove x264 libx264-dev
+
+sudo apt-get install build-essential checkinstall cmake pkg-config yasm
+sudo apt-get install git gfortran
+sudo apt-get install libjpeg8-dev libjasper-dev libpng12-dev
+
+sudo apt-get install libtiff5-dev
+
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
+sudo apt-get install libxine2-dev libv4l-dev
+sudo apt-get install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+sudo apt-get install qt5-default libgtk2.0-dev libtbb-dev
+sudo apt-get install libatlas-base-dev
+sudo apt-get install libfaac-dev libmp3lame-dev libtheora-dev
+sudo apt-get install libvorbis-dev libxvidcore-dev
+sudo apt-get install libopencore-amrnb-dev libopencore-amrwb-dev
+sudo apt-get install x264 v4l-utils
+
+sudo apt-get install libprotobuf-dev protobuf-compiler
+sudo apt-get install libgoogle-glog-dev libgflags-dev
+sudo apt-get install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
+
+udo apt-get install python-dev python-pip python3-dev python3-pip
+sudo -H pip2 install -U pip numpy
+sudo -H pip3 install -U pip numpy
+
+git clone https://github.com/opencv/opencv.git
+git clone https://github.com/opencv/opencv_contrib.git
+
+cd opencv
+mkdir build
+cd build
+
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D BUILD_DOCS=OFF \
+-D WITH_TBB=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON \
+-D WITH_V4L=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D WITH_NVCUVID=ON -D \
+WITH_CUDA=ON -D WITH_CSTRIPES=ON -D CMAKE_INSTALL_PREFIX=/usr/local/ \
+-D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules -D BUILD_EXAMPLES=ON ..
+
+make -j4
+sudo make install
+sudo ldconfig
