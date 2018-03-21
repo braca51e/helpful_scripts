c012c71aa05eab07a22847f88ff7f9440481ed06
braca51e <braca51e@gmail.com>
2018-03-20 22:42:21 -0400

    Install pcl

---
+++ install_pcl.sh
@@ -0,0 +1,6 @@
+#Follow instruction
+https://hackmd.io/s/rkUygOq8g#
+#install sensor for openni that work but needs fixing
+https://github.com/avin2/SensorKinect
+#fix
+https://github.com/avin2/SensorKinect/pull/5/commits/94d6ed38751571051b3acbdad4ea874f41deb375
