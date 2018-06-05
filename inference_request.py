f7d5eca8d292347259168444cd2b145bf87c88dd
braca51e <braca51e@gmail.com>
2018-06-05 18:12:04 -0400

    Update inference_request.py

--- inference_request.py
+++ inference_request.py
@@ -7,7 +7,7 @@ import ssl
 
 headers = {"Content-type": "application/json",
            "X-Access-Token": "YOUR-CLOUD-TOKEN"}
-conn = httplib.HTTPSConnection("dev.sighthoundapi.com", 
+conn = httplib.HTTPSConnection("dev.we-page.com", 
        context=ssl.SSLContext(ssl.PROTOCOL_TLSv1))
 
 # To use a hosted image uncomment the following line and update the URL
@@ -17,7 +17,7 @@ conn = httplib.HTTPSConnection("dev.sighthoundapi.com",
 #image_data = base64.b64encode(open("/path/to/local/image.jpg").read())
 
 params = json.dumps({"image": image_data})
-conn.request("POST", "/v1/detections?type=face,person&faceOption=landmark,gender", params, headers)
+conn.request("POST", "/v1/endpoint", params, headers)
 response = conn.getresponse()
 result = response.read()
 print "Detection Results = " + str(result)
