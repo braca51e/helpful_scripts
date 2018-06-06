8f5c50599e4b315fe2340a32b3bfc9c2e85aa74e
braca51e <braca51e@gmail.com>
2018-06-06 14:56:40 -0400

    Update add_margin_bbox.py

--- add_margin_bbox.py
+++ add_margin_bbox.py
@@ -1,6 +1,6 @@
-def add_margin_bbox(bbox, img_size, margin_percent_x=0.01, margin_percent_y=0.05):
-    margin_x = margin_percent_x*bbox[1]
-    margin_y = margin_percent_y*bbox[0]
+def add_margin_bbox(bbox, img_size, margin_percent_x=0.10, margin_percent_y=0.05):
+    margin_x = margin_percent_x*(bbox[2] - bbox[0]) #Width
+    margin_y = margin_percent_y*(bbox[3] - bbox[1]) #Height
     bbox[0] = int(np.maximum(bbox[0]-margin_x, 0))
     bbox[1] = int(np.maximum(bbox[1]-margin_y, 0))
     bbox[2] = int(np.minimum(bbox[2]+margin_x, img_size[1]))
