e7eee8f72818a0393ee2d231957bf9531f403592
braca51e <braca51e@gmail.com>
2019-02-09 22:09:36 -0500

    Update dockerCommands.sh

--- dockerCommands.sh
+++ dockerCommands.sh
@@ -1,9 +1,9 @@
 nvidia-docker rmi -f 6ecab9e3b344
 nvidia-docker images
 #build image
-nvidia-docker build -t potato .
+nvidia-docker build -t iamge_name .
 #Run image
-nvidia-docker run -v $(pwd)/test_dir:/opt/ml -it potato
+nvidia-docker run -v $(pwd)/test_dir:/opt/ml -it iamge_name
 #Delete images
 nvidia-docker rm $(nvidia-docker ps -a -f status=exited -q)
 nvidia-docker rmi -f $(nvidia-docker images -a -q)
@@ -21,7 +21,7 @@ nvidia-docker cp <containerId>:/file/path/within/container /host/path/target
 
 
 #Launch open port
-nvidia-docker run -v $(pwd)/test_dir:/opt/ml -p 8080:8080 --rm mickey_band serve
+nvidia-docker run -v $(pwd)/test_dir:/opt/ml -p 8080:8080 --rm image_name serve
 
 #Remove <none> images dangling 
 nvidia-docker images -qf dangling=true | xargs --no-run-if-empty nvidia-docker rmi
